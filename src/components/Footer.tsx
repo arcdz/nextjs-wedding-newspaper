9708b6c75dbd578a41fa98b2cbb7d89987131e50
Andrei Rotariu <rotariu.c.andrei@gmail.com>
2023-04-11 16:09:52 +0300

    preparing

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+// import DarkModeToggle from './DarkModeToggle'
+
+const Footer = () => {
+  return (
+    <footer className="flex w-full justify-end border-t  p-4">
+      Footer
+    </footer>
+  )
+}
+
+export default Footer
